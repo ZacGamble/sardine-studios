5368e7bdda373e5f34eb7ee614b19d9e77644d76
Zachary <zacrgamble@gmail.com>
2022-09-27 14:04:46 -0600

    vue branch initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ScheduleView from './ScheduleView.vue';
+</script>
+
+<template>
+  <main>
+    <ScheduleView/>
+  </main>
+</template>
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,78 @@
+<template>
+    <main>
+        <div id="portfolio" class="row p-3 text-center">
+            <h2>Samples of my work:</h2>
+        </div>
+        <div class="row">
+
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/7.jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/Fan Art.jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/299209377_111674371648013_6400373296916303089_n.jpg"
+                        alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/Gingerbread Doodle (1).jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/Lily.jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/NTC Color JPG (2).jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/Raniya.jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid"
+                        src="src/assets/tumblr_d1cc1973ba5107a7d065f6d703b100f4_7b3cbaae_2048.jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/Tumblr_l_42087897995588.jpg" alt="artwork">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="p-2 shadow rounded">
+                    <img class="art img-fluid" src="src/assets/298962132_799751964389284_561950820838317439_n.jpg"
+                        alt="artwork">
+                </div>
+            </div>
+        </div>
+        <hr>
+    </main>
+</template>
+
+
+<script>
+export default {
+    setup(){
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
